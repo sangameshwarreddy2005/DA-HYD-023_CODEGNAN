0e4514ef70ba8b233a639d54ef200de5f3baeead
sangameshwarreddy <sangameshbandari3@gmail.com>
2026-07-28 16:11:03 +0530

    Tokens-Day1

---
+++ day1.py
@@ -0,0 +1,44 @@
+#print("Hello world")
+'''
+multi line comments triple qouties
+
+->keywords
+->identifiers
+->Literals
+->operators
+-.punctuators
+
+# we have the 35 keywords
+#help() -->ArithmeticError keywords -->it gives list of 35 keywords -->True,False.....
+#identifiers -->this are the names being alloted to variables ,functions clasess....
+#we cannot start with a numbers
+name="Sangameshwar Reddy"
+print(name)
+email_id='sangameshbandari3@gmail.com'
+print(email_id)# python is case-sensitive -->NameError email-->Email
+#$batch=23--> No special characters
+#True=45-->No keywords
+#1name="sanga"
+#email id="sam@gmial.com" dont use the space
+batch="da23"
+print(batch)
+#variabls
+batch='iot'
+print(batch)
+
+#Literals -->These are the constants (values(s))-->number,character
+
+
+#Operators--> Operators performs specific operation -->7types
+
+lent=5
+brd=10
+area=lent*brd
+print(area)
+'''
+
+#Multiline assingment of variables
+Name,Branch,Year="Sangamesh","IOT",2026
+print(Name)
+print(Branch)
+print(Year)
